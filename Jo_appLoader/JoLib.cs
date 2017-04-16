30801ba47cb3eab74686ee863a5e3c5eb462fd8a
Koorosh <Mr.Oplus@yahoo.fr>
2017-04-16 05:50:49 +0430

    Add project files.

---
+++ Jo_appLoader/JoLib.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JoApp_Loader
+{
+    class JoLib
+    {
+        public static string string_0 = "REGISTER";
+        public static string string_1 = "LOGIN";
+        public static string string_2 = "GETCHARGE";
+        public static string string_3 = "NEW_PROJECT";
+        public static string string_4 = "ALLOW";
+        public static string string_5 = "ALLOWFREE";
+        public static string string_6 = "SUCCESS_PROJECT";
+        public static string string_7 = "TEMP";
+        public static string string_8 = "SUCCESS_REQUEST_IOS";
+        public static string string_9 = "NEW_REQUEST_IOS";
+        public static string GenerateNewProject()
+        {
+
+            //{ "GyOU":"GxIKK1OFG0cSD1D=","pt==":"ZN==","L3Mk":"ZmxjAwH1"}
+            string format = "\"{0}\":\"{1}\",";
+            string data = "{";
+            data += String.Format(format, EncodeToServer("ACT"), "GxIKK1OFG0cSD1D=");
+            data += String.Format(format, EncodeToServer("e"), EncodeToServer("0"));
+            data += String.Format(format, "L3Mk", "ZmxjAwpl");
+            data = data.Substring(0, data.Length - 1);
+            data += "}";
+            return data;
+        }
+        public static string GenerateTempPacket()
+        {
+            //{ "GyOU":"IRIAHN==","pt==":"ZGZlZwV=","JxMH":"2XwMulQLe9zR24mMuPQMuqv02daMuAva2Xbt2LUMughZVAva2LKndqva2LLt2X\/LfqhZ2XsMtqvdVAvh2YUMvAvf24jtDIOYVAzV2XmMvAviVAzT2X\/Lc9vk2X8hVAvb2Xpt2o7LgAvd24mLdAva2LoowPQLdgzS2XsLflQLdAdi24mLfqhZ2X8="}
+            string format = "\"{0}\":\"{1}\",";
+            string data = "{";
+            data += String.Format(format, EncodeToServer("ACT"), "IRIAHN==");
+            data += String.Format(format, EncodeToServer("e"), EncodeToServer("0"));
+            // data += String.Format(format, EncodeToServer("isFree"), EncodeToServer("synfr"));
+            // data += String.Format(format, EncodeToServer("pid"), EncodeToServer("12458"));
+
+            data += String.Format(format, "qzMGMKWl", "MzSfp2H=");
+            data += String.Format(format, "L3Mk", "ZmxjAwpl");
+            data = data.Substring(0, data.Length - 1);
+            data += "}";
+            return data;
+        }
+        public static string GenerateSuccessProject()
+        {
+            //{"GyOU":"H1IQD0IGH19DHx9XEHAH","pt==":"ZN=="}
+            string format = "\"{0}\":\"{1}\",";
+            string data = "{";
+            data += String.Format(format, EncodeToServer("ACT"), "H1IQD0IGH19DHx9XEHAH");
+            data += String.Format(format, EncodeToServer("e"), EncodeToServer("0"));
+            data = data.Substring(0, data.Length - 1);
+            data += "}";
+            return data;
+        }
+        public static string GenerateAllowPacket()
+        {
+            string format = "\"{0}\":\"{1}\",";
+            string data = "{";
+            data += String.Format(format, EncodeToServer("ACT"), EncodeToServer("ARJ_CEBWRPG"));
+            data += String.Format(format, EncodeToServer("e"), EncodeToServer("0"));
+            data += String.Format(format, EncodeToServer("pid"), EncodeToServer("390672"));
+            data += String.Format(format, EncodeToServer("lic"), EncodeToServer("0"));
+            data += String.Format(format, EncodeToServer("l"), EncodeToServer("0"));
+
+            data = data.Substring(0, data.Length - 1);
+            data += "}";
+            return data;
+        }
+        public static string GenerateLoginPacket()
+        {
+            string format = "\"{0}\":\"{1}\",";
+            string data = "{";
+            data += String.Format(format, EncodeToServer("ACT"), "GR9UFH4=");
+            data += String.Format(format, EncodeToServer("e"), EncodeToServer("0"));
+            data = data.Substring(0, data.Length - 1);
+            data += "}";
+            return data;
+        }
+        public static string GenerateRemainingDays()
+        {
+            string format = "\"{0}\":\"{1}\",";
+            string data = "{";
+            data += String.Format(format, EncodeToServer("ACT"), EncodeToServer("TRGPUNETR"));
+            data += String.Format(format, EncodeToServer("e"), EncodeToServer("0"));
+            data += String.Format(format, EncodeToServer("charge"), EncodeToServer("100"));
+            data += String.Format(format, EncodeToServer("umsg"), EncodeToServer("0"));
+            data += String.Format(format, EncodeToServer("expiredate"), EncodeToServer("0"));
+            data += String.Format(format, EncodeToServer("countday"), EncodeToServer("999"));
+            data += String.Format(format, EncodeToServer("lastpaycode"), EncodeToServer("0"));
+            data = data.Substring(0, data.Length - 1);
+            data += "}";
+            return data;
+        }
+        public static string DecodeFromServer(string txt)
+        {
+            return Encode(FromBase64(Encode(txt)));
+        }
+        public static string EncodeToServer(string txt)
+        {
+            return Encode(ToBase64(Encode(txt)));
+        }
+        public static string EncodePassword(string txt)
+        {
+            return Encode(ToBase64(ToBase64(txt)));
+        }
+        public static string ToBase64(string str)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(str));
+        }
+        public static string FromBase64(string str)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(str));
+        }
+        public static string Encode(string str)
+        {
+            char[] array = str.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int num = (int)array[i];
+                if (num >= 97 && num <= 122)
+                {
+                    if (num > 109)
+                    {
+                        num -= 13;
+                    }
+                    else
+                    {
+                        num += 13;
+                    }
+                }
+                else if (num >= 65 && num <= 90)
+                {
+                    if (num > 77)
+                    {
+                        num -= 13;
+                    }
+                    else
+                    {
+                        num += 13;
+                    }
+                }
+                array[i] = (char)num;
+            }
+            return new string(array);
+        }
+        static Random r = new Random();
+
+    }
+}
---
+++ Jo_appLoader/Program.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.IO;
+namespace JoApp_Loader
+{
+    class Program
+    {
+        public static ManualResetEvent tcpClientConnected = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[*] By Mr.8ThBiT :)");
+           // Process.Start("http://8thbit.net/"); 
+            bool appflag = true;
+            try
+            {   
+                string hostfile = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+                hostfile = Path.Combine(hostfile, @"system32\drivers\etc\hosts");
+                string[] lines = File.ReadAllLines(hostfile);
+                bool flag = false;
+                bool flag2 = false;
+                foreach (string line in lines)
+                {
+                    if (line.Trim().StartsWith("#"))
+                        continue;
+                    if (line.Contains("bejo.ir"))
+                    {
+                        Console.WriteLine("[+] Host file is already patched.");
+                        flag = true;
+                        break; 
+                    }
+                }
+                if (flag == false )
+                {
+                    try
+                    {
+                        File.AppendAllText(hostfile, Environment.NewLine + "127.0.0.1\tbejo.ir\r\n");
+                        Console.WriteLine("[+] Host file were patched.");
+                    }
+                    catch (Exception e2)
+                    {
+                        Console.WriteLine("[-] Unable to add record to host file.");
+                        appflag = false;
+                    }
+                }
+                Run("netsh.exe int ip delete addr 1 78.46.19.104/32 ");
+                Run("netsh.exe int ip delete addr 1 78.46.19.104 ");
+                Run("netsh.exe int ip add addr 1 78.46.19.104/32 st=ac sk=tr");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[-] " + e.Message);
+                appflag = false;
+            }
+            
+            Console.WriteLine("[+] Starting application on port 80");
+            TcpListener listener = new TcpListener(IPAddress.Any,80);
+            try
+            {
+                start(listener); 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[-] Port is busy ...\n[-] Trying to Kill Port");
+                try
+                {
+                    var prc = new ProcManager();
+                    if (prc.KillByPort(80))
+                    {
+                        Console.WriteLine("[+] Trying to sit on port 80.");
+                        start(listener); 
+                    }
+                   
+                }
+                catch (Exception e2)
+                {
+                    appflag = false;
+                    Console.WriteLine("[-] Unable to kill port 80.");
+                }
+            }
+            try
+            {
+                Process.Start("JoApp.exe");
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("[-] Unable to find JoApp executable file,\r\n\ttry to copy the loader in JoApp folder :)");
+                if(appflag)
+                    Console.WriteLine("[+] however Everything is working fine you can execute JoApp.exe manually");
+            }
+            Console.WriteLine("[*] Press Enter Or ESC to exit");
+            while (true)
+            {
+
+                ConsoleKeyInfo x = Console.ReadKey();
+                if (x.Key == ConsoleKey.Escape || x.Key == ConsoleKey.Enter)
+                    Environment.Exit(0); 
+            }
+        }
+        private static void Run(string command)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + command;
+            startInfo.Verb = "runas";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+        static void start(TcpListener listener)
+        {
+            listener.Start();
+            startResponding(listener);
+        }
+        static void startResponding(TcpListener listener)
+        {
+            listener.BeginAcceptTcpClient(new AsyncCallback(AcceptConnection), listener); 
+        }
+        static Random rnd = new Random();
+        private static void AcceptConnection(IAsyncResult ar)
+        {
+            TcpListener listener = (TcpListener)ar.AsyncState;
+            TcpClient client = listener.EndAcceptTcpClient(ar);
+            Console.WriteLine("[+] Connection Accepted From : {0}" , client.Client.RemoteEndPoint);
+            string str = "" ;
+            if (client.GetStream().CanRead)
+            {
+                byte[] myReadBuffer = new byte[1024];
+                StringBuilder myCompleteMessage = new StringBuilder();
+                int numberOfBytesRead = 0;
+
+                // Incoming message may be larger than the buffer size. 
+                do
+                {
+                    numberOfBytesRead = client.GetStream().Read(myReadBuffer, 0, myReadBuffer.Length);
+
+                    myCompleteMessage.AppendFormat("{0}", Encoding.UTF8.GetString(myReadBuffer, 0, numberOfBytesRead));
+
+                }
+                while (client.GetStream().DataAvailable);
+
+                // Print out the received message to the console.
+                str = myCompleteMessage.ToString();
+            }
+            string code = "";
+            string url = Regex.Match(str, "(POST|GET) (.*) HTTP/1.1", RegexOptions.IgnoreCase).Groups[2].Value;
+            if (url == "/service_joapp/v2/LOGIN.php")
+            {
+                code = JoLib.GenerateLoginPacket();
+            } else if (url == "/service_joapp/v2/GETCHARGE.php")
+            {
+                code = JoLib.GenerateRemainingDays();
+            } else if (url == "/service_joapp/v2/NEW_PROJECT.php")
+            {
+                code = JoLib.GenerateNewProject(); 
+            } else if (url == "/service_joapp/v2/ALLOW.php")
+            {
+                code = JoLib.GenerateAllowPacket();
+            } else if (url == "/service_joapp/v2/TEMP.php")
+            {
+                code = JoLib.GenerateTempPacket();
+            } else if (url == "/service_joapp/v2/SUCCESS_PROJECT.php")
+            {
+                code = JoLib.GenerateSuccessProject();
+            } else if (url.Contains("/downloads/version_joapp.php"))
+            {
+                code = "\r\n<br><br>Cracked By 8ThBiT";
+            }
+
+            
+            Console.WriteLine("[+] Responding to {0}" , url);
+            
+
+
+          //  code = Encoding.UTF8.GetString(Convert.FromBase64String(code));
+           // code = code.Replace("{0}", randomMail());
+            string  packet =  "HTTP/1.1 200 OK\r\n";
+            packet += "Connection: Keep-Alive\r\n";
+            packet += "Content-Length: " + code.Length + "\r\n";
+            packet += "Content-Type: text/html;\r\n";
+            packet += "Keep-Alive: timeout=5, max=50\r\n\r\n";
+            packet += code;
+            byte[] packetStub = System.Text.Encoding.UTF8.GetBytes(packet);
+            if (client.Connected)
+            {
+                client.GetStream().Write(packetStub, 0, packetStub.Length);
+                client.GetStream().Flush();
+                client.GetStream().Close();
+            }
+            startResponding(listener);
+        }
+    }
+    public class PRC
+    {
+        public int PID { get; set; }
+        public int Port { get; set; }
+        public string Protocol { get; set; }
+    }
+    public class ProcManager
+    {
+        public bool KillByPort(int port)
+        {
+            var processes = GetAllProcesses();
+            if (processes.Any(p => p.Port == port))
+                try
+                {
+                    Process.GetProcessById(processes.First(p => p.Port == port).PID).Kill();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[-] " + ex.Message);
+                    return false;
+                }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<PRC> GetAllProcesses()
+        {
+            var pStartInfo = new ProcessStartInfo();
+            pStartInfo.FileName = "netstat.exe";
+            pStartInfo.Arguments = "-a -n -o";
+            pStartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            pStartInfo.UseShellExecute = false;
+            pStartInfo.RedirectStandardInput = true;
+            pStartInfo.RedirectStandardOutput = true;
+            pStartInfo.RedirectStandardError = true;
+
+            var process = new Process()
+            {
+                StartInfo = pStartInfo
+            };
+            process.Start();
+
+            var soStream = process.StandardOutput;
+
+            var output = soStream.ReadToEnd();
+            if (process.ExitCode != 0)
+                throw new Exception("[-] Internal Error");
+
+            var result = new List<PRC>();
+
+            var lines = Regex.Split(output, "\r\n");
+            foreach (var line in lines)
+            {
+                if (line.Trim().StartsWith("Proto"))
+                    continue;
+
+                var parts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var len = parts.Length;
+                if (len > 2)
+                    result.Add(new PRC
+                    {
+                        Protocol = parts[0],
+                        Port = int.Parse(parts[1].Split(':').Last()),
+                        PID = int.Parse(parts[len - 1])
+                    });
+
+
+            }
+            return result;
+        }
+    }
+}
